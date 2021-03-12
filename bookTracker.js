e5a494b33c05f1dc1501bdbeb53c4b994fd6580d
thasty1707 <tim.hasty@gmail.com>
2021-03-12 12:24:26 -0600

    Kept listbooks; changed addBookToLibrary to append html table

--- bookTracker.js
+++ bookTracker.js
@@ -8,19 +8,19 @@ function Book(title, author, bookLength, haveReadYet){
 };
 
 //Test book data
-const theHobbit = myLibrary.push(new Book("The Hobbit","JRR Tolkien",274,"Yes"));
-const readyPlayer1 = myLibrary.push(new Book("Ready Player One", "Ernest Cline", 372, "Yes"));
+const theHobbit = myLibrary.push(new Book("The Hobbit","JRR Tolkien","274 pages","Yes"));
+const readyPlayer1 = myLibrary.push(new Book("Ready Player One", "Ernest Cline", "372 pages", "Yes"));
 
 //Function to add new books to the Library
 function addBookToLibrary(){    
     //Pull values of input fields
     let bookTitle = document.getElementById("bookTitle").value;
     let bookAuthor = document.getElementById("bookAuthor").value;
-    let bookLength = document.getElementById("bookLength").value;
+    let bookLength = String(document.getElementById("bookLength").value + " pages");
     let readStatus = document.getElementById("readYet").value;
 
-    //Check input and use values to push new object into myLibrary object
-    if(bookTitle == ""){
+     //Check input and use values to push new object into myLibrary object
+    /* if(bookTitle == ""){
         alert("Please enter a title for the new book!");
         return;
     }else if(bookAuthor == ""){
@@ -31,11 +31,31 @@ function addBookToLibrary(){
         return;
     }else{
         myLibrary.push(new Book(bookTitle, bookAuthor, bookLength, readStatus));
-    };
+    }; */
 
+    myLibrary.push(new Book(bookTitle, bookAuthor, bookLength, readStatus));
+
+    let bookTable = document.getElementById("tableOfBooks");
+    let newRow = document.createElement("tr");
+    newRow.className = "bookRow";
+    let newBook = myLibrary[myLibrary.length - 1];
+    let bookProperties = ['title', 'author', 'pages', 'readIt'];
+    
+    firstCell = document.createElement('td');
+    firstCell.innerHTML = (myLibrary.length);
+    newRow.appendChild(firstCell);
+    
+    for(let i = 0; i < bookProperties.length; i++){
+
+        cell = document.createElement('td');
+        cell.innerHTML = newBook[bookProperties[i]];
+        newRow.appendChild(cell);
+    };
+    bookTable.appendChild(newRow);
 
     //Reset input fields
     resetInput();
+
     console.log(myLibrary);
 };
 
@@ -50,7 +70,6 @@ function resetInput(){
 };
 
 function listBooks(){
-    
     //Variables for the table to list books on page
     let bookTable = document.getElementById("tableOfBooks");
     let libLength = myLibrary.length;
@@ -77,12 +96,12 @@ function listBooks(){
 }; 
 listBooks();
 
-//Function to clear table before refilling?
-function emptyTable(){
+//Function to clear table before refilling? Causes bad latency
+/* function emptyTable(){
     let bookTable = document.getElementById("tableOfBooks");
     let rowCount = bookTable.childElementCount;
-    while(rowCount > 0){bookTable.removeChild}
-};
+    while(rowCount > 1){bookTable.removeChild}
+}; */
 
 //Remove spaces from a title
 // let titleId1 = myLibrary[0].title.replace(/\s+/g, '');

0592eb9233f36c066b39fd109deeae57c2814979
thasty1707 <tim.hasty@gmail.com>
2021-03-17 17:33:32 -0500

    Reset functions; working properly again so far

--- bookTracker.js
+++ bookTracker.js
@@ -12,11 +12,11 @@ function Book(title, author, bookLength, haveReadYet){
 
 //Test book data
 
-// const theHobbit = myLibrary.push(new Book("The Hobbit","JRR Tolkien","274 pages", "<input type='checkbox' checked=true;>"));
-// const readyPlayer1 = myLibrary.push(new Book("Ready Player One", "Ernest Cline", "372 pages", "<input type='checkbox' checked=true;>"));
+const theHobbit = myLibrary.push(new Book("The Hobbit","JRR Tolkien","274 pages", "<input type='checkbox' checked=true;>"));
+const readyPlayer1 = myLibrary.push(new Book("Ready Player One", "Ernest Cline", "372 pages", "<input type='checkbox' checked=true;>"));
 
 //Function to add new books to the Library
-function addBookToLibrary(){    
+function addBookToLibrary(){   
     //Pull values of input fields
     let bookTitle = document.getElementById("bookTitle").value;
     let bookAuthor = document.getElementById("bookAuthor").value;
@@ -45,70 +45,41 @@ function addBookToLibrary(){
         myLibrary.push(new Book(bookTitle, bookAuthor, bookLength, readStatus));
     };
 
-    // let bookTable = document.getElementById("tableOfBooks");
-    // let newRow = document.createElement("tr");
-    // newRow.className = "bookRow";
-    // newRow.setAttribute('id',(myLibrary.length - 1))
-    // let newBook = myLibrary[myLibrary.length - 1];
-    // let bookProperties = ['title', 'author', 'pages'];
+    let bookTable = document.getElementById("tableOfBooks");
+    let newRow = document.createElement("tr");
+    newRow.className = "bookRow";
+    newRow.setAttribute('id',(myLibrary.length - 1))
+    let newBook = myLibrary[myLibrary.length - 1];
+    let bookProperties = ['title', 'author', 'pages'];
     
-    // firstCell = document.createElement('td');
-    // firstCell.innerHTML = (myLibrary.length);
-    // newRow.appendChild(firstCell);
+    firstCell = document.createElement('td');
+    firstCell.innerHTML = (myLibrary.length);
+    newRow.appendChild(firstCell);
     
-    // for(let i = 0; i < (bookProperties.length + 1); i++){
-    //     cell = document.createElement('td');
-    //     cell.innerHTML = newBook[bookProperties[i]];
-    //     newRow.appendChild(cell);
-    // };
-
-    // if(readStatus == "true"){
-    //     cell.innerHTML = "<input type='checkbox' id='checkbox' checked=true;>";
-    //     newRow.appendChild(cell);
-    // }else{
-    //     cell.innerHTML = "<input type='checkbox';>";
-    //     newRow.appendChild(cell);
-    // };
-
-    // let lastCell = document.createElement('td');
-    // lastCell.innerHTML = "<button class='deleteBook';><strong>X</strong></button>";
-    // newRow.appendChild(lastCell);
-
-    // bookTable.appendChild(newRow);
-    createBookTable();
-    //Reset input fields after new entry
-    resetInput();
-};
-
-
-const createBookTable = () => {
-
-    while (bookTable.firstChild){
-        bookTable.removeChild(bookTable.firstChild);
+    for(let i = 0; i < (bookProperties.length + 1); i++){
+        cell = document.createElement('td');
+        cell.innerHTML = newBook[bookProperties[i]];
+        newRow.appendChild(cell);
     };
 
-    let newBookTable = document.createElement('table');
-    newBookTable.setAttribute('id', 'tableOfBooks');
+    if(readStatus == "true"){
+        cell.innerHTML = "<input type='checkbox' id='checkbox' checked=true;>";
+        newRow.appendChild(cell);
+    }else{
+        cell.innerHTML = "<input type='checkbox';>";
+        newRow.appendChild(cell);
+    };
 
-    let bookTableHead = document.createElement('thead');
-    bookTableHead.className = 'bookHeaders';
+    let lastCell = document.createElement('td');
+    lastCell.innerHTML = "<button class='deleteBook';><strong>X</strong></button>";
+    newRow.appendChild(lastCell);
 
-    let bookTableHeaderRow = document.createElement('tr');
-    bookTableHeaderRow.className = 'bookRow';
+    bookTable.appendChild(newRow);
+   
 
-    let bookHeaders = ["Book No\.", "Title", "Author", "Length", "Read", "Delete"]
-    bookHeaders.forEach(header => {
-        let bookHeader = document.createElement('th');
-        bookHeader.innerText = header;
-        bookTableHeaderRow.appendChild(bookHeader);
-    });
-
-    bookTableHead.append(bookTableHeaderRow);
-    bookTable.append(bookTableHead);
-
-    let bookTableBody = document.createElement('tbody');
-    bookTableBody.className = 'bookTableBody';
-    
+    // listCurrentBooks();
+    //Reset input fields after new entry
+    resetInput();
 };
 
 //Function to clear form fields; called after adding new Book to Library
@@ -152,11 +123,10 @@ function listCurrentBooks(){
         bookTable.appendChild(newRow);
     };
 }; 
-// listCurrentBooks();
 
-function deleteBook(){
-    
-}
+listCurrentBooks();
+
+// function deleteBook(){};
 
 //Remove spaces from a title
 // let titleId1 = myLibrary[0].title.replace(/\s+/g, '');
